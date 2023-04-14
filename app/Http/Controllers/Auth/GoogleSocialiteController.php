276a0999d090d174b718db75fdd1c9efa6c1a910
abol ghana <aghp81@gmail.com>
2023-04-14 16:50:44 +0430

    پس از لاگین یا رجیستر وارد dashboard

--- app/Http/Controllers/Auth/GoogleSocialiteController.php
+++ app/Http/Controllers/Auth/GoogleSocialiteController.php
@@ -37,7 +37,7 @@ class GoogleSocialiteController extends Controller
       
                 Auth::login($finduser);
      
-                return redirect('/home');
+                return redirect('/dashboard');
       
             }else{
                 $newUser = User::create([
@@ -50,7 +50,7 @@ class GoogleSocialiteController extends Controller
      
                 Auth::login($newUser);
       
-                return redirect('/home');
+                return redirect('/dashboard');
             }
      
         } catch (Exception $e) {
