9c1e391aba20d8922f59e1d36f26014450c28c11
abol ghana <aghp81@gmail.com>
2023-04-14 17:20:25 +0430

    حل مشکل لاگین با کاربر ثبت نام کرده با ایمیل

--- app/Http/Controllers/Auth/GoogleSocialiteController.php
+++ app/Http/Controllers/Auth/GoogleSocialiteController.php
@@ -28,33 +28,27 @@ class GoogleSocialiteController extends Controller
     public function handleCallback()
     {
         try {
-     
             $user = Socialite::driver('google')->user();
-      
-            $finduser = User::where('social_id', $user->id)->first();
-      
-            if($finduser){
-      
-                Auth::login($finduser);
-     
-                return redirect('/dashboard');
-      
-            }else{
-                $newUser = User::create([
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'social_id'=> $user->id,
-                    'social_type'=> 'google',
-                    'password' => encrypt('my-google')
-                ]);
-     
-                Auth::login($newUser);
-      
-                return redirect('/dashboard');
-            }
-     
-        } catch (Exception $e) {
-            dd($e->getMessage());
+        } catch (\Exception $e) {
+            return redirect('login');
         }
+    
+        // check if they're an existing user
+        $existingUser = User::where('email', $user->email)->first();
+        if($existingUser){
+            // log them in
+            auth()->login($existingUser, true);
+        } else {
+            // create a new user
+            $newUser                  = new User;
+            $newUser->name            = $user->name;
+            $newUser->email           = $user->email;
+            $newUser->google_id       = $user->id;
+            $newUser->avatar          = $user->avatar;
+            $newUser->avatar_original = $user->avatar_original;
+            $newUser->save();
+            auth()->login($newUser, true);
+        }
+        return redirect()->to('/dashboard');
     }
 }
