f3221235f3eee8ddb5999edcd33470437258b153
abol ghana <aghp81@gmail.com>
2023-04-21 11:31:38 +0430

    ایجاد جدول فروشنده

--- database/migrations/2023_04_21_064216_create_shops_table.php
+++ database/migrations/2023_04_21_064216_create_shops_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('user_id'); // for user_id
+            $table->string('title')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('telephone');
+            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
