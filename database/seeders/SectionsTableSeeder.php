c5bbb9cf92f9552232dafef1f70d14a4d4868898
abol ghana <aghp81@gmail.com>
2023-06-04 11:31:54 +0430

    ایجاد سیدر برای اینسزت اطلاعات در  جدول سکشن

---
+++ database/seeders/SectionsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SectionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
